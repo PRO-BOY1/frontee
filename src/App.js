2a83426641fec85a711d9e73ee0575d0c25ccd8c
PRO-BOY1 <157917472+PRO-BOY1@users.noreply.github.com>
2025-02-18 20:24:23 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-// src/App.js
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/login" component={LoginPage} />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
