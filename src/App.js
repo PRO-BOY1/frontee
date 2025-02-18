425ad71243939c34f521c667f909148076532725
PRO-BOY1 <157917472+PRO-BOY1@users.noreply.github.com>
2025-02-18 20:27:05 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
+import Dashboard from './Dashboard';
 import LoginPage from './LoginPage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
